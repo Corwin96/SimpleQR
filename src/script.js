6fbc888308e19814216c7aea9fa845467fff203f
Corwin <33621431+Corwin96@users.noreply.github.com>
2023-03-21 13:38:59 +0100

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,9 @@
+let totalClicks = 0;
+
+const buttonClick = () => {
+    if (document.getElementById("checkbox").checked) {
+        alert("Oeps..");
+    } else {
+        alert("Accepteer gevaren om door te gaan")
+    }
+}
